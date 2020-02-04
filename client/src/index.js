fdd0be5d4f4a335bfd17442a088890fc6d83574e
vBoBv <vannponhvath@gmail.com>
2020-02-04 18:32:58 +1300

    added client

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "redux";
+
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.querySelector("#root"));
