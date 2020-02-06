74e7edafc999ab1e772049ec94b3cc37b84c7202
vBoBv <vannponhvath@gmail.com>
2020-02-07 09:55:25 +1300

    added side-nav

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Header = () => {
-    return <div className='top-header'>Header</div>;
+    return (
+        <div className='top-header'>
+            <div className='ui label top-header__user-info'>
+                <i className='user outline icon heading-2'></i>{" "}
+                <span className='heading-2'>Ponhvath</span>
+            </div>
+            <div className='logo heading-1'>Fast horse</div>
+            <div className='top-header__sign-in-button'>
+                <button className='ui teal button'>
+                    <span className='heading-2'>Sign In / Sign Up</span>
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default Header;
--- client/src/components/NavSidebar.js
+++ client/src/components/NavSidebar.js
@@ -1,7 +1,35 @@
 import React from "react";
 
 const NavSidebar = () => {
-    return <div className='nav-sidebar'>NavSidebar</div>;
+    return (
+        <div className='nav-sidebar'>
+            <nav className='sidebar'>
+                <ul className='side-nav'>
+                    <li className='side-nav__item side-nav__item--active'>
+                        <a href='#' className='side-nav__link'>
+                            <span>Menu</span>
+                        </a>
+                    </li>
+                    <li className='side-nav__item'>
+                        <a href='#' className='side-nav__link'>
+                            <span>Contact</span>
+                        </a>
+                    </li>
+                    <li className='side-nav__item'>
+                        <a href='#' className='side-nav__link'>
+                            <span>Create New Menu</span>
+                        </a>
+                    </li>
+                    <li className='side-nav__item'>
+                        <a href='#' className='side-nav__link'>
+                            <span>All Menu</span>
+                        </a>
+                    </li>
+                </ul>
+                <div className='legal'>&copy; All rights reserved.</div>
+            </nav>
+        </div>
+    );
 };
 
 export default NavSidebar;
