452fc10e581e2261f30751c8cc61382d464942b6
vBoBv <vannponhvath@gmail.com>
2020-02-08 20:54:59 +1300

    added create menu route handler

--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -1,3 +1,25 @@
+const mongoose = require("mongoose");
+
+const requireLogin = require("../middlewares/requireLogin");
+
+const Menu = mongoose.model("menus");
+
 module.exports = (app) => {
-    app.post("/api/menus", (req, res) => {});
+    app.post("/api/menus", requireLogin, async (req, res) => {
+        const { title, price, description } = req.body;
+
+        const menu = new Menu({
+            title,
+            price,
+            description,
+            _user: req.user.id
+        });
+        try {
+            const savedMenu = await menu.save();
+
+            res.send(savedMenu);
+        } catch (err) {
+            res.status(422).send(err);
+        }
+    });
 };
