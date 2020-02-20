7978049ea99a4f9c368892300e76a5ef9ffa71e0
vBoBv <vannponhvath@gmail.com>
2020-02-20 22:31:16 +1300

    tweaked MenuDelete

--- client/src/components/menus/MenuDelete.js
+++ client/src/components/menus/MenuDelete.js
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Modal from '../Modal';
+import fetchMenus from '../../actions';
 
-const MenuDelete = () => {
-    return <div>Menu Delete</div>;
+class MenuDelete extends Component {
+	componentDidMount() {
+		this.props.fetchMenus();
+	}
+
+	renderActions() {
+		const { id } = this.props.match.params;
+
+		return (
+			<React.Fragment>
+				<button
+					onClick={() => this.props.deleteStream(id)}
+					className='ui button negative'>
+					Delete
+				</button>
+				<Link to='/' className='ui button'>
+					Cancel
+				</Link>
+			</React.Fragment>
+		);
+	}
+
+	renderContent() {
+		if (!this.props.selectedMenu) {
+			return 'Are you sure you want to delete this stream?';
+		}
+
+		return `Are you sure you want to delete the stream with title: ${this.props.selectedMenu.menuName}`;
+	}
+
+	render() {
+		return (
+			<Modal
+				title='Delete Menu'
+				content={this.renderContent()}
+				actions={this.renderActions()}
+				onDismiss={() => history.push('/')}
+			/>
+		);
+	}
+}
+
+const mapStateToProps = (state, ownProps) => {
+	return { selectedMenu: state.menu[ownProps.match.params.id] };
 };
 
-export default MenuDelete;
+export default connect(mapStateToProps)(MenuDelete);
