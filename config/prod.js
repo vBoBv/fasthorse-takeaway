5ebc833fe85927c9b00cc85e2d5a7b93d8396c2c
vBoBv <vannponhvath@gmail.com>
2020-02-25 23:01:11 +1300

    added prod key

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,10 @@
 module.exports = {
-    googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    mongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY
+	googleClientID: process.env.GOOGLE_CLIENT_ID,
+	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+	facebookClientID: process.env.FACEBOOK_CLIENT_ID,
+	facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+	linkedInClientID: process.env.LINKEDIN_CLIENT_ID,
+	linkedInClientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+	mongoURI: process.env.MONGO_URI,
+	cookieKey: process.env.COOKIE_KEY
 };
